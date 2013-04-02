f2660c44687d2e20d5d14dfe67ce1a67845ba79b
Jeffrey Hu <zhiwehu@gmail.com>
2013-04-02 22:07:43 +0800

    add flatpages and account urls

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
@@ -7,6 +8,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
+
+    url(r'^accounts/', include('allauth.urls')),
 
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
@@ -18,3 +21,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                                'document_root': settings.MEDIA_ROOT,
+                                }),
+                            )
+
+urlpatterns += patterns('django.contrib.flatpages.views',
+                        (r'^page/(?P<url>.*)$', 'flatpage'),
+                        )
